3ecc5eccc1391aa7d6a619152b6c4fe5d3d61e6c
Ethan Carollo <118255407+EthanCarollo@users.noreply.github.com>
2024-10-23 19:04:06 +0200

    Feat : add board game

---
+++ Assets/Scripts/Game/Board/Board.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    private int width = 11; 
+    private int height = 7; 
+    public GameObject tilePrefab;  
+    public GameObject[,] boardArray; 
+
+    void Start()
+    {
+        boardArray = new GameObject[width, height];
+        CreateBoard();
+    }
+
+    void CreateBoard()
+    {
+        float tileWidth = 1.0f;
+        float tileHeight = 1.0f;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                float posX = x * tileWidth;
+                float posY = y * tileHeight;
+                Vector3 position = new Vector3(posX, posY, 0);
+                GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, this.transform);
+                tile.name = $"Tile {x},{y}";
+                boardArray[x, y] = tile;
+            }
+        }
+    }
+
+}
