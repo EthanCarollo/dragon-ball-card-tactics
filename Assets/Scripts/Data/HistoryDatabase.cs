3a403b13f7da930ff6f01381343a7627a2808289
Ethan Carollo <etcarollo@gmail.com>
2025-02-02 17:47:38 +0100

    Fix : save history in file

--- Assets/Scripts/Data/HistoryDatabase.cs
+++ Assets/Scripts/Data/HistoryDatabase.cs
@@ -1,15 +1,15 @@
+using System;
+using System.IO;
 using System.Linq;
 using UnityEngine;
-using UnityEditor;
-using System;
 
 [CreateAssetMenu(fileName = "HistoryDatabase", menuName = "History/HistoryDatabase")]
 public class HistoryDatabase : ScriptableObject
 {
     private static HistoryDatabase _instance;
+    private static string FilePath => Path.Combine(Application.persistentDataPath, "history.json");
 
-    public HistoryFight[] history;
-
+    public HistoryFight[] history = new HistoryFight[0];
 
     public static HistoryDatabase Instance
     {
@@ -21,26 +21,64 @@ public class HistoryDatabase : ScriptableObject
                 if (_instance == null)
                 {
                     Debug.LogError("HistoryDatabase instance not found in Resources folder!");
+                    _instance = CreateInstance<HistoryDatabase>();
+                }
+                else
+                {
+                    _instance.LoadHistory();
                 }
             }
             return _instance;
         }
     }
 
-    public void AddFight(CharacterContainer[] characters, int round, int seconds){
+    public void AddFight(CharacterContainer[] characters, int round, int seconds)
+    {
         var historyList = history.ToList();
         historyList.Add(new HistoryFight(characters, round, seconds));
         history = historyList.ToArray();
+        SaveHistory();
+    }
+
+    public void SaveHistory()
+    {
+        try
+        {
+            string json = JsonUtility.ToJson(this, true);
+            File.WriteAllText(FilePath, json);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to save history: {e.Message}");
+        }
+    }
+
+    public void LoadHistory()
+    {
+        if (File.Exists(FilePath))
+        {
+            try
+            {
+                string json = File.ReadAllText(FilePath);
+                JsonUtility.FromJsonOverwrite(json, this);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Failed to load history: {e.Message}");
+            }
+        }
     }
 }
 
 [Serializable]
-public class HistoryFight {
+public class HistoryFight
+{
     public CharacterContainer[] characters;
     public int round;
     public int seconds;
 
-    public HistoryFight(CharacterContainer[] characters, int round, int seconds){
+    public HistoryFight(CharacterContainer[] characters, int round, int seconds)
+    {
         this.characters = characters;
         this.round = round;
         this.seconds = seconds;
