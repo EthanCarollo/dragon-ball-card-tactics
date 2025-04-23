c60faf0b4295b54d8c2a320c379595459cf66c06
Ethan Carollo <etcarollo@gmail.com>
2025-04-23 12:45:15 +0200

    Feat : add details of synergy in character container

--- Assets/Scripts/Game/Character/Synergy/SynergyCharacterShowPrefabScript.cs
+++ Assets/Scripts/Game/Character/Synergy/SynergyCharacterShowPrefabScript.cs
@@ -1,16 +1,35 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SynergyCharacterShowPrefabScript : MonoBehaviour {
+public class SynergyCharacterShowPrefabScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     public Synergy synergy;
     public TextMeshProUGUI synergyNumber;
     public Image synergyImage;
+    public RectTransform synergyDescriptionContainer;
+    public TextMeshProUGUI synergyDescriptionText;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        synergyDescriptionContainer.gameObject.SetActive(true);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(synergyDescriptionContainer);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        synergyDescriptionContainer.gameObject.SetActive(false);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(synergyDescriptionContainer);
+    }
 
     public void Setup(Synergy synergy) {
         this.synergy = synergy;
         synergyNumber.text = synergy.synergyName;
         synergyImage.sprite = synergy.synergyImage;
+        synergyDescriptionText.text = synergy.GetDescription();
+        synergyDescriptionText.maskable = false;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(synergyDescriptionContainer);
+        synergyDescriptionContainer.gameObject.SetActive(false);
     }
 
 }
